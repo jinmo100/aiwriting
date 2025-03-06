c15c189567df95657615687f5b0c3ff5aa072030
Jinmo <omnij100@gmail.com>
2025-03-06 18:59:05 +0800

    Initial commit

---
+++ src/main/java/com/jinmo/aiwriting/AiwritingApplication.java
@@ -0,0 +1,13 @@
+package com.jinmo.aiwriting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiwritingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiwritingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jinmo/aiwriting/AiwritingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jinmo.aiwriting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiwritingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
